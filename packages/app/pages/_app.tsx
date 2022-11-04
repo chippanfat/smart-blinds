4d618ccc06b04c7db62c58b83a10dcc7df2a4539
Ethan Flynn-Harding <efh@deliowealth.com>
2022-11-04 15:39:30 +0000

    Added clerk and protected pages with a placeholder for a single page for login

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,15 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+} from "@clerk/nextjs";
 import Layout from "app/components/layout";
 import { CLERK_API_KEY } from "app/config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider frontendApi={CLERK_API_KEY}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SignedIn>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton afterSignInUrl="/device" afterSignUpUrl="/device" />
+      </SignedOut>
     </ClerkProvider>
   );
 }
